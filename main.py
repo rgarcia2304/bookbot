61148c486bb1701b4e8280d1f85849692659a399
rgarcia2304 <rgarcia22141@gmail.com>
2025-08-19 14:46:06 -0400

    added functionallity to pass books through the system arguments

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import *
+import sys
+
 
 def get_book_text(fp):
     with open(fp) as f:
@@ -8,7 +10,10 @@ def get_book_text(fp):
 
     
 def main():
-    str_book = get_book_text("books/frankenstein.txt")
+    if(len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    str_book = get_book_text(sys.argv[1])
     word_count = get_book_word_count(str_book)
     ranked_dicts = format_count(count_frequency(str_book))
     #print(f"{word_count} words found in the document")
