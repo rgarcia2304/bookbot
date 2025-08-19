5b24cce00f76b8ca4a31e75cacf1bdf91f9d946e
rgarcia2304 <rgarcia22141@gmail.com>
2025-08-19 14:39:30 -0400

    FEATURE: Print formatting now included for the characters in the book

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ def get_book_text(fp):
 def main():
     str_book = get_book_text("books/frankenstein.txt")
     word_count = get_book_word_count(str_book)
-    print(f"{word_count} words found in the document")
-    print(count_frequency(str_book))
+    ranked_dicts = format_count(count_frequency(str_book))
+    #print(f"{word_count} words found in the document")
+    #print(count_frequency(str_book))
+    #print(ranked_dicts)
+
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for val in ranked_dicts:
+        print(f"{val} \n ")
+
 main()
--- stats.py
+++ stats.py
@@ -17,3 +17,21 @@ def count_frequency(book_string):
         else:
             count_dict[char] = 1
     return count_dict
+
+def get_value(d):
+    return list(d.values())[0]
+
+def format_count(char_dict):
+    lst_of_char_info = []
+    
+    for key in char_dict:
+        new_dict ={}
+        new_dict[key] = char_dict[key]
+        #new_dict = dict(key = char_dict[key])
+        lst_of_char_info.append(new_dict)
+
+    lst_of_char_info.sort(reverse=True, key=get_value)
+    
+    return lst_of_char_info
+
+
