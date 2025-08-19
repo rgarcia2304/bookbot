d617bc7bc29e6741a15c1b2d76fbc1ecb23c263e
rgarcia2304 <rgarcia22141@gmail.com>
2025-08-19 13:55:48 -0400

    FEATURE: added word count functionality

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ def get_book_text(fp):
 
     return str(file_contents)
 
+def get_book_word_count(book_string):
+    count = 0
+    split_book_string = book_string.split(" ")
+
+    for i in split_book_string:
+        count += 1
+    
+    return count 
+    
 def main():
     str_book = get_book_text("books/frankenstein.txt")
-    print(str_book)
+    word_count = get_book_word_count(str_book)
+    return print(f"{word_count} words found in the document")
 
 main()
